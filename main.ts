b7162dda60f98c6e399b81f3f62e22be10455773
bryan weinstein <bryanweinstein@outlook.com>
2021-10-28 12:22:50 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-let grade = 0
-input.onButtonPressed(Button.A, function () {
-    grade = 1
-})
 function alarm (duration: number) {
     for (let index = 0; index < duration; index++) {
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -10,15 +6,8 @@ function alarm (duration: number) {
         basic.pause(500)
     }
 }
-input.onButtonPressed(Button.B, function () {
-    grade = 0
-})
 basic.forever(function () {
     if (input.temperature() > 35 || input.temperature() < 14) {
-        if (grade == 0) {
-            alarm(5)
-        } else {
-            alarm(randint(1, 5))
-        }
+        alarm(randint(1, 5))
     }
 })
